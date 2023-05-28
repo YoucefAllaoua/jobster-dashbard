013639b828c615cf99b6774ead81c4fdf77b2cfd
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-28 17:48:30 +0100

    youcef

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+const Landing = () => {
+	return (
+		<div>
+			<h1>Landing</h1>
+		</div>
+	);
+};
+export default Landing;
