cac8a32e373123e7350565f18b984fa55324a50a
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-29 17:52:53 +0100

    youcef

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "normalize.css";
 import "./index.css";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
