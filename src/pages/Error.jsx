21cc0b53e4bccc692234c88a19304e957e0e474d
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-29 11:40:41 +0100

    youcef

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,16 @@
+import { NavLink } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
-}
-export default Error
+	return (
+		<Wrapper className="full-page">
+			<div className="">
+				<img src={img} alt="not found" />
+				<h3>Oooh ! something went Wrong</h3>
+				<p>text</p>
+				<NavLink to="/">back Home</NavLink>
+			</div>
+		</Wrapper>
+	);
+};
+export default Error;
