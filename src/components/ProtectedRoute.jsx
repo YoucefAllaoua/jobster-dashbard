5806d591d79912774f9cf0f0f5c2ef07e7f46430
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-29 14:15:06 +0100

    youcef

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }) => {
 	const navigate = useNavigate();
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	if (!isLoggedIn) {
-		return <Navigate to="/" />;
-	} else {
 		return <Navigate to="/landing" />;
+	} else {
+		return children;
 	}
 };
 export default ProtectedRoute;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
 const Register = () => {
+	const [values, setValues] = useState(initialState);
+	const handleChange = () => {};
+	const submitHandler = () => {};
 	return <div>Register</div>;
 };
 export default Register;
