b3ddabde4cec541a9394a9a08ac99ff788f81932
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-29 11:00:53 +0100

    youcef

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
-import Landing from "./pages/Landing";
-import styled from "styled-components";
+import Landing from "./pages";
 
 // when we use this styled component we will have a button (usual one in our page with a uniq class generated auto)
 // so we can have many items that return all the same node element but we will not have name conflict because each one have a uniq class
