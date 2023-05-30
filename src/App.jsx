d7a29fb50dc6d6d7bd91654de4fb9bdd8b874a26
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-30 17:47:51 +0100

    youcef

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import SharedLayout from "./pages/Dashboard/SharedLayout";
-import Stats from "./pages/Dashboard/Stats";
+import { AddJob, AllJobs, Profile, SharedLayout, Stats } from "./pages/Dashboard";
+
 // when we use this styled component we will have a button (usual one in our page with a uniq class generated auto)
 // so we can have many items that return all the same node element but we will not have name conflict because each one have a uniq class
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/dashboard" element={<SharedLayout />}>
+				<Route
+					path="/"
+					element={
+						<ProtectedRoute>
+							<SharedLayout />
+						</ProtectedRoute>
+					}
+				>
 					<Route index element={<Stats />} />
+					<Route path="profile" element={<Profile />} />
+					<Route path="all-jobs" element={<AllJobs />} />
+					<Route path="add-jobs" element={<AddJob />} />
 				</Route>
 				<Route path="landing" element={<Landing />} />
 				<Route path="register" element={<Register />} />
--- src/pages/Dashboard/SharedLayout.jsx
+++ src/pages/Dashboard/SharedLayout.jsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const SharedLayout = () => {
-  return (
-    <div>SharedLayout</div>
-  )
-}
-export default SharedLayout
+	return (
+		<div>
+			<h2>this is shared </h2>
+			<Outlet />
+		</div>
+	);
+};
+export default SharedLayout;
