9766c30ba7c263dcbccbe25c5e9d86c7c719ae67
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-31 15:42:27 +0100

    youcef

--- src/components/BigSideBar.jsx
+++ src/components/BigSideBar.jsx
@@ -1,6 +1,23 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import Logo from "./Logo";
+import { NavLinks } from "../components";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleSideBar } from "../Features/Sidebar/SideBarSlice";
 
 const BigSideBar = () => {
-	return <Wrapper>BigSideBar</Wrapper>;
+	const dispatch = useDispatch();
+	const { isSideBarOpen } = useSelector((store) => store.sideBar);
+	return (
+		<Wrapper>
+			<div className={isSideBarOpen ? "sidebar-container show-sidebar" : "sidebar-container "}>
+				<div className="content">
+					<header>
+						<Logo />
+					</header>
+					<NavLinks toggle={() => dispatch(toggleSideBar())} />
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 export default BigSideBar;
