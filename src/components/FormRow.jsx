c135af3141e9c85a94f0efaec42721ada4c05961
Youcef_Allaoua <y_allaoua@estin.dz>
2023-05-31 16:30:49 +0100

    youcef

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -2,7 +2,7 @@ const FormRow = ({ name, type, value, labelText, handleChange }) => {
 	return (
 		<div className="form-row">
 			<label htmlFor={name} className="form-label">
-				{labelText}
+				{labelText ? labelText : name}
 			</label>
 			<input type={type} name={name} value={value} onChange={handleChange} className="form-input" />
 		</div>
--- src/pages/Dashboard/Profile.jsx
+++ src/pages/Dashboard/Profile.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import FormRow from "../../components/FormRow";
+import { Wrapper } from "../../assets/wrappers/DashboardFormPage";
+import { toast } from "react-toastify";
+
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
-export default Profile
+	const { user } = useSelector((store) => store.user);
+	const useInfo = useState({
+		name: user?.name || "",
+		email: user?.email || "",
+		location: user?.location || "",
+		lastName: user?.lastName || "",
+	});
+	return <div>Profile</div>;
+};
+export default Profile;
