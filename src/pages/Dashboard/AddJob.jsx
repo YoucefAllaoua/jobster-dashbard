f85a58d821d7a0a55192334c220fe4128f9ed726
Youcef_Allaoua <y_allaoua@estin.dz>
2023-06-01 16:13:57 +0100

    youcef

--- src/pages/Dashboard/AddJob.jsx
+++ src/pages/Dashboard/AddJob.jsx
@@ -1,11 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { addSingleJob, editJob } from "../../Features/job/jobSlice";
- 
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { FormRow } from "../../components";
+
 const AddJob = () => {
 	const { isLoading, position, company, jobLocation, jobType, jobTypeOptions, status, statusOptions, isEditing, editJobId } = useSelector((store) => store.job);
 	const dispatch = useDispatch();
-	const handleSubmit = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
 		if (!position || !company || !jobLocation) {
 			toast.error("please fill out all fields!");
 			return;
@@ -13,13 +16,32 @@ const AddJob = () => {
 		if (isEditing) {
 			dispatch(editJob());
 		} else {
-			dispatch(addJob());
+			dispatch(addSingleJob());
 		}
 	};
 	const handleJobInput = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
 	};
-	return <div>AddJob</div>;
+	return (
+		<Wrapper>
+			<form onSubmit={handleSubmit} className="form">
+				<h3>{isEditing ? "Edit job" : "Add job"}</h3>
+				<div className="form-center">
+					<FormRow type="text" name="position" value={position} handleChange={handleJobInput} />
+					<FormRow type="text" name="company" value={company} handleChange={handleJobInput} />
+					<FormRow type="text" name="jobLocation" value={jobLocation} labelText="job Location" handleChange={handleJobInput} />
+					<div className="btn-container">
+						<button onClick={() => console.log("clear")} className=" btn btn-block clear-btn " type="button">
+							clear
+						</button>
+						<button className=" btn btn-block submit-btn " type="submit">
+							clear
+						</button>
+					</div>
+				</div>
+			</form>
+		</Wrapper>
+	);
 };
 export default AddJob;
