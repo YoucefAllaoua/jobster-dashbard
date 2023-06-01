1910948b570bdab4f3314e578db4a8462bfb29e9
Youcef_Allaoua <y_allaoua@estin.dz>
2023-06-01 13:22:28 +0100

    youcef

--- src/Features/job/jobSlice.js
+++ src/Features/job/jobSlice.js
@@ -16,6 +16,7 @@ const initialState = {
 	editJobId: "",
 };
 
+const addJob = createAsyncThunk("addJob")
 const jobSlice = createSlice({
 	name: "job",
 	initialState,
--- src/pages/Dashboard/AddJob.jsx
+++ src/pages/Dashboard/AddJob.jsx
@@ -1,4 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
 const AddJob = () => {
+	const { isLoading, position, company, jobLocation, jobType, jobTypeOptions, status, statusOptions, isEditing, editJobId } = useSelector((store) => store.job);
+	const dispatch = useDispatch();
+	const handleSubmit = () => {
+		if (!position || !company || !jobLocation) {
+			toast.error("please fill out all fields!");
+			return;
+		}
+		if (isEditing) {
+			dispatch(editJob());
+		} else {
+			dispatch(addJob());
+		}
+	};
 	return <div>AddJob</div>;
 };
 export default AddJob;
