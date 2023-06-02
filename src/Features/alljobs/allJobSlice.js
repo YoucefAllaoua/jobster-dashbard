3fe2af03e4d1d87b574917b3d849db0cef3c6a58
Youcef_Allaoua <y_allaoua@estin.dz>
2023-06-02 19:34:18 +0100

    youcef

---
+++ src/Features/alljobs/allJobSlice.js
@@ -0,0 +1,21 @@
+const { createSlice } = require("@reduxjs/toolkit");
+const initialFilterState = {
+	search: "",
+	searchStatus: "all",
+	searchType: "all",
+	sort: "latest",
+	sortOptions: ["latest", "oldest", "a-z", "z-a"],
+};
+const initialState = {
+	isLoading: false,
+	jobs: [],
+	totalJobs: 0,
+	numOfPages: 1,
+	page: 1,
+	monthlyApplications: [],
+	...initialFilterState,
+};
+const allJobsSlice = createSlice({
+	name: "allJobs",
+	initialState,
+});
