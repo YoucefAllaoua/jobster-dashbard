51cd1769478372b26be0798f21c93815f7e2e2e3
Youcef_Allaoua <y_allaoua@estin.dz>
2023-07-05 10:56:49 +0100

    youcef

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -1,4 +1,23 @@
+import { useSelector } from "react-redux";
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import { useState } from "react";
+
 const ChartsContainer = () => {
-  return <div>Charts</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button
+        type="button"
+        onClick={() => setBarChart(!barChart)}
+      >
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 export default ChartsContainer;
